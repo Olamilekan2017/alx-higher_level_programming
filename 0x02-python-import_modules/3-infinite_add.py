522c61c6e3cad90cb8567741a939426740e79523
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-04 19:47:22 -0800

    infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     sumint = 0
     x = len(argv) - 1
-    if x < 1:
-        print("{}".format(x))
-    else:
-        for i in range(x):
-            sumint += int(argv[i + 1])
-        print("{:d}".format(sumint))
+    for i in range(x):
+        sumint += int(argv[i + 1])
+    print("{:d}".format(sumint))
