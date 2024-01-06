87bcaed8b99391cf672c5df6d04c29f74ef1967a
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-06 15:57:42 -0800

    1-element_at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     temp = len(my_list)
     if idx < 0:
         return None
-    elif idx > temp:
+    elif idx >= temp:
         return None
     else:
         return my_list[idx]
