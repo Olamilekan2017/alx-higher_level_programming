f31a5a2a4789016bafd26d3616268aa5d8185304
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-06 17:26:55 -0800

    new_in_list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    modified_list = my_list[:]
+    temp =len(my_list)
+    if idx < 0:
+        return modified_list
+    if idx >= temp:
+        return modified_list
+    else:
+        modified_list[idx] = element
+        return modified_list
