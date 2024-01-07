4bf545dd265a5443ed9d7fc1a6584819827e49c9
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-07 04:09:49 -0800

    multiple_return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    length = len(sentence)
+    first = sentence[0]
+    if length == 0:
         return 0, None
-    elif len(sentence) > 0:
-        return len(sentence), sentence[0]
+    elif length > 0:
+        return len(sentence), first
