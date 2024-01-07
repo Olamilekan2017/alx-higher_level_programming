5207550ff6b49c04904c9cbd585aeba1beaf8469
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-07 13:27:27 -0800

    no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyton3
 def no_c(my_string):
-    if my_string:
+    if my_string[:]:
         new_string = my_string.translate({ord("c"): None})
         new_new_string = new_string.translate({ord("C"): None})
         return new_new_string
