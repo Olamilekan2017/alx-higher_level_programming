8a5987022b78d2d51a1a8dd96a36487cfd08d3bb
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-10 03:11:40 -0800

    uniq_add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if my_list:
+        unique_list = set()
+        for num in my_list:
+            unique_list.add(num)
+        return sum(unique_list)
