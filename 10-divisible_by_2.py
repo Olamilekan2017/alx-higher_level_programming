32205795175344e12f8e6139ad0f137b8785eeb2
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-10 03:33:02 -0800

    only_diff_elements

--- 10-divisible_by_2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    if my_list:
-        new_list = []
-        for num in my_list:
-            multiple_of_2 = num % 2 == 0
-            new_list.append(multiple_of_2)
-        return new_list
