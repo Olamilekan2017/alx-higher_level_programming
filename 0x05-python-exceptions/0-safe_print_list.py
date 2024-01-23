b8c409b89543df818414012cd779feeccea52caf
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-22 19:30:21 -0800

    safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        total = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            total += 1
+        print()
+        return total
+    except IndexError:
+        print()
+        return total
