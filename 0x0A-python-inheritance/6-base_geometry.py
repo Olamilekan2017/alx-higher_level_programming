59fa2d5c79bdd3f16bd8e73b46729258446a3e48
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-02-05 17:28:33 -0800

    base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This function writes an empty list """
+
+
+class BaseGeometry:
+    """ Represent the class basegeometry """
+    
+    def area(self):
+        raise Exception("area() is not implemented")
