bf2ec375ae4033572793edcd490c3882d9b6ba8d
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-02-07 03:11:28 -0800

    append_after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line """
+    txt = ""
+    with open(filename) as i:
+        for line in i:
+            txt += line
+            if search_string in line:
+                txt += new_string
+                with open(filename, "w") as j:
+                    j.write(txt)
