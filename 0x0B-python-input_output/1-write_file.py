08158e050cdf2e5f19eaf0c7938140b294a8a37b
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-02-07 00:48:19 -0800

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """ write a string to the the file """
 
-    with open(filenane, "w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
