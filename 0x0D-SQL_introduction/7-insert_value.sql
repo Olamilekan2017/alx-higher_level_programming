cb4da395d931fde039eb2ce293aae074b433c821
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-03-14 01:16:25 -0700

    insert_value

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,2 @@
 -- Inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-INSERT INTO `first_table` (
-	`id` VALUES 89,
-	`name` VALUES Best School
-);
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
