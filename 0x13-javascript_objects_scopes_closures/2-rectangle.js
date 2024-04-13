ce89a35c8f8b442b4b827c82bd344f40bd258340
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-04-13 01:37:59 -0700

    Rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,12 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
       return {};
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
