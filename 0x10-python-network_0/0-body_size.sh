8de6e93879e83cd9a9025ca4a4f965617e024720
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-04-26 11:29:40 -0700

    body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -o /dev/null -w '%{size_download}\n' http://localhost:5000
+curl -sI "$1" | grep -oiE "Content-Length: [0-9]+" | cut -d ' ' -f2
