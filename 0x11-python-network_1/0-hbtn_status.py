f591caa7b704344551514366bf0d587f3b96254d
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-04-26 23:11:53 -0700

    htbn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as ur:
         if ur.read():
             urldata = ur.read()
+            utf8_content = urldata.decode('utf-8')
             print("Body response:")
-            print(f"\t- type: {type(urldata)}")
-            print(f"\t- content: {urldata}")
-            print(f"\t- utf8 content: {urldata.decode('utf-8')}")
+            print("\t- type: {}".format(type(urldata)))
+            print("\t- content: {}".format(urldata))
+            print("\t- utf8 content: {}".format(utf8_content))
