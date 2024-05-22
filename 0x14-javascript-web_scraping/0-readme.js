e3b1dcd2202f95f1f3a3548c658ede51cdb6d540
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-05-21 22:02:30 -0700

    readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+const fs = require('fs');
+if (process.argv.length > 2) {
+  fs.readFile(process.argv[2], (error, flcnt) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(flcnt.toString('utf-8'));
+    }
+  });
+}
