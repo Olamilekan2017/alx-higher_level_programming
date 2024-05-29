0f8c9a2a3674ad3774187095155bb5929cd90c1b
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-05-29 16:59:07 -0700

    100-script

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// JavaScript script that updates the text color of the <header> element to red (#FF0000)
+'use strict';
+window.addEventListener('load', () => {
+  document.querySelector('header').style.color = 'red';
+});
